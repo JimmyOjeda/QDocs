8a410e67e0743a1bab746b2518bde7913eefe0a2
danieljpay <danielj.pay@hotmail.com>
2022-11-04 22:34:06 -0600

    QDOC-35 view create dictionary improves

--- src/app/views/dictionary-builder/dictionary-builder.component.ts
+++ src/app/views/dictionary-builder/dictionary-builder.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dictionary-builder.component.css']
 })
 export class DictionaryBuilderComponent implements OnInit {
+  table = [
+    "Id",
+    "Nombre",
+    "Dirección",
+    "Teléfono",
+    "Escuela",
+    "Apodo",
+    "Edad"
+  ];
+
+  // tempArray: any = [];
+  tokens: any = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  tableToTokens(index: number) {
+    this.tokens.push(this.table[index]);
+    this.table = this.table.filter(item => item !== this.table[index] )
+  }
+
+  tokensToTable(index: number) {
+    this.table.push(this.tokens[index]);
+    this.tokens = this.tokens.filter((item: any) => item !== this.tokens[index]);
   }
 
 }
