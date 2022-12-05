a505b402a0c6b8806ef9c5ac7901694374b7927e
danieljpay <A14004346@alumnos.uady.mx>
2022-12-04 22:26:21 -0600

    dictionary view using services

--- src/app/views/dictionary-view/dictionary-view.component.ts
+++ src/app/views/dictionary-view/dictionary-view.component.ts
@@ -1,26 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ManageDictionariesService } from 'src/app/services/manage-dictionaries/manage-dictionaries.service';
+import { ManageEntriesService } from 'src/app/services/manage-entries/manage-entries.service';
 
 @Component({
-  selector: 'app-dictionary-view',
-  templateUrl: './dictionary-view.component.html',
-  styleUrls: ['./dictionary-view.component.css']
+    selector: 'app-dictionary-view',
+    templateUrl: './dictionary-view.component.html',
+    styleUrls: ['./dictionary-view.component.css']
 })
 export class DictionaryViewComponent implements OnInit {
 
-  foods = [
-    {
-      value: 'steak',
-      viewValue: 'Steak'
-    },
-    {
-      value: 'pizza',
-      viewValue: 'Pizza'
-    },
-  ]
+    dictionaries: any = [];
+    entries: any = [];
+    dictionaryIdSelected = '';
 
-  constructor() { }
+    constructor(
+        private dictionaryService: ManageDictionariesService,
+        private entryService: ManageEntriesService
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.dictionaryService.readAllDictionaries()
+        .subscribe(
+            response => this.dictionaries = response.data,
+            error => console.log(JSON.stringify(error))
+        )
+    }
 
+    loadEntries() {
+        this.entryService.readAllEntries()
+        .subscribe(
+            response => {
+                this.entries = response.data.filter(
+                    entry => entry.dictionary === this.dictionaryIdSelected
+                );
+            },
+            error => console.log(JSON.stringify(error))
+        )
+    }
 }
